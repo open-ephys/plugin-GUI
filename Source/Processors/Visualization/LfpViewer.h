0e9fff51b59c77593eeab4a8b2d76652a3997809
jsiegle <jsiegle@mit.edu>
2012-02-19 17:40:39 -0500

    Added headers to obsolete visualizers

--- Source/Processors/Visualization/LfpViewer.h
+++ Source/Processors/Visualization/LfpViewer.h
@@ -1,13 +1,25 @@
 /*
-  ==============================================================================
+    ------------------------------------------------------------------
 
-    LfpViewer.h
-    Created: 16 Aug 2011 8:46:57pm
-    Author:  jsiegle
+    This file is part of the Open Ephys GUI
+    Copyright (C) 2012 Open Ephys
 
-  ==============================================================================
+    ------------------------------------------------------------------
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 */
-
 #ifndef __LFPVIEWER_H_7FEACF46__
 #define __LFPVIEWER_H_7FEACF46__
 
@@ -36,7 +48,11 @@
 #define GL_BGRA_EXT 0x80e1
 #endif
 
+/**
+  
+  --THIS FILE IS OBSOLETE, BUT REMAINS FOR REFERENCE PURPOSES--
 
+*/
 
 class LfpViewer : public Renderer
 {
