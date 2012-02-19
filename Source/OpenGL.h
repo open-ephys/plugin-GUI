4eff318876388b8ee88911720c5a4ce5c55adea2
jsiegle <jsiegle@mit.edu>
2012-02-19 16:44:20 -0500

    Added file to automatically include OpenGL-related header files

---
+++ Source/OpenGL.h
@@ -0,0 +1,28 @@
+
+/*
+  General header for files that use OpenGL
+*/
+
+#ifdef _WIN32
+#include <windows.h>
+#endif
+
+#if JUCE_WINDOWS
+#include <gl/gl.h>
+#include <gl/glu.h>
+#elif JUCE_LINUX
+#include <GL/gl.h>
+#include <GL/glut.h>
+#undef KeyPress
+#elif JUCE_IPHONE
+#include <OpenGLES/ES1/gl.h>
+#include <OpenGLES/ES1/glext.h>
+#elif JUCE_MAC
+#include <GLUT/glut.h>
+#endif
+
+#ifndef GL_BGRA_EXT
+#define GL_BGRA_EXT 0x80e1
+#endif
+
+#include <FTGL/ftgl.h>
