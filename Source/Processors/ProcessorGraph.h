45c2ab5b538488691ebbc79d36241a2d7e3908eb
jsiegle <jsiegle@mit.edu>
2012-02-19 17:01:53 -0500

    Added headers to all processor source code

--- Source/Processors/ProcessorGraph.h
+++ Source/Processors/ProcessorGraph.h
@@ -1,17 +1,45 @@
 /*
-  ==============================================================================
+    ------------------------------------------------------------------
 
-    ProcessorGraph.h
-    Created: 30 Apr 2011 8:36:35pm
-    Author:  jsiegle
+    This file is part of the Open Ephys GUI
+    Copyright (C) 2012 Open Ephys
 
-  ==============================================================================
+    ------------------------------------------------------------------
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 */
 
 #ifndef __PROCESSORGRAPH_H_124F8B50__
 #define __PROCESSORGRAPH_H_124F8B50__
 
 #include "../../JuceLibraryCode/JuceHeader.h"
+
+/**
+  
+  Owns all processors and constructs the signal chain.
+
+  The GUI revolves around the ProcessorGraph, which enables the user to 
+  dynamically update the signal chain. This object creates and deletes
+  all of the processors that handle data, and holds the rules for connecting
+  them prior to data acquisition.
+
+  @see FilterViewport, GenericProcessor, GenericEditor, RecordNode,
+       AudioNode, Configuration, MessageCenter
+
+*/
+
 
 class GenericProcessor;
 class RecordNode;
@@ -53,10 +81,6 @@ public:
 
 	void saveState();
 	void loadState();
-	//const String saveState(const File& file);
-	//const String loadState(const File& file);
-
-	//XmlElement* createNodeXml(GenericProcessor*);
 
 	int getNextFreeAudioChannel();
 	int getNextFreeRecordChannel();
