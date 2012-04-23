f17257dd78f29b52e90b5c14ddd46e0bbd4ad447
Josh Siegle <jsiegle@mit.edu>
2012-04-23 13:51:08 -0400

    Minor changes to FPGAThread

--- Source/Processors/DataThreads/FPGAThread.h
+++ Source/Processors/DataThreads/FPGAThread.h
@@ -52,9 +52,9 @@ public:
 	FPGAThread(SourceNode* sn);
 	~FPGAThread();
 
-	bool foundInputSource(); // {return true;}
-	int getNumChannels();// {return 32;}
-	float getSampleRate();// {return 25000.0;}
+	bool foundInputSource() {return true;}
+	int getNumChannels() {return 32;}
+	float getSampleRate() {return 25000.0;}
 	
 private:
 
