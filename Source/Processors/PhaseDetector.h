e36119a1ffcd67aa066a9261329b4dca076affb7
jsiegle <jsiegle@mit.edu>
2013-03-21 17:49:04 -0400

    Change copyright year to 2013
    Use the following command to update the copyright year in all files,
    starting in the Source/ directory:
    
    find . -type f -print0 | xargs -0
       sed -i 's/Copyright (C) 2012/Copyright (C) 2013/g'
    
    Works like a charm!

--- Source/Processors/PhaseDetector.h
+++ Source/Processors/PhaseDetector.h
@@ -2,7 +2,7 @@
     ------------------------------------------------------------------
 
     This file is part of the Open Ephys GUI
-    Copyright (C) 2012 Open Ephys
+    Copyright (C) 2013 Open Ephys
 
     ------------------------------------------------------------------
 
