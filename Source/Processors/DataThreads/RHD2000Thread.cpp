f6a62024a551ffb7498d67ce6279001e67da51f1
jsiegle <jsiegle@mit.edu>
2013-05-21 19:55:03 -0400

    Fix incorrect bitvolts value in RHD2000Thread

--- Source/Processors/DataThreads/RHD2000Thread.cpp
+++ Source/Processors/DataThreads/RHD2000Thread.cpp
@@ -412,8 +412,8 @@ float RHD2000Thread::getSampleRate()
 
 float RHD2000Thread::getBitVolts()
 {
-    //return 0.1907;
-	return 0.000064; // empirical value determined by calibrating against reid's code - fix this
+
+	return 0.195f;//0.000064; // empirical value determined by calibrating against reid's code - fix this
 }
 
 double RHD2000Thread::setUpperBandwidth(double desiredUpperBandwidth)
