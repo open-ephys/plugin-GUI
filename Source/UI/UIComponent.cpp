5a68da81e0ae76633f6fec7c3f8b0e3b458ba198
jsiegle <jsiegle@mit.edu>
2013-06-18 21:27:06 -0400

    Remove Custom FPGA from ProcessorList

--- Source/UI/UIComponent.cpp
+++ Source/UI/UIComponent.cpp
@@ -92,7 +92,7 @@ UIComponent::UIComponent(MainWindow* mainWindow_, ProcessorGraph* pgraph, AudioC
     mainWindow->setMenuBar(this);
 #endif
 
-    //getEditorViewport()->loadState(File("/home/jsiegle/Programming/GUI/Builds/Linux/build/spike_display_2.xml"));
+    //getEditorViewport()->loadState(File("/home/jsiegle/Programming/GUI/Builds/Linux/build/test.xml"));
 
 }
 
