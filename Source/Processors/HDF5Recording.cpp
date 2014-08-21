16452b04caf92a9c11f3ea720dc34e24cb7fba22
Aaron Cuevas Lopez <aacuelo@teleco.upv.es>
2014-08-21 04:39:48 +0200

    Add Recordengine files to Introjucer

--- Source/Processors/HDF5Recording.cpp
+++ Source/Processors/HDF5Recording.cpp
@@ -1,12 +1,26 @@
 /*
-==============================================================================
-
-HDF5Recording.cpp
-Created: 21 Jul 2014 1:03:17am
-Author:  Kether
-
-==============================================================================
-*/
+ ------------------------------------------------------------------
+ 
+ This file is part of the Open Ephys GUI
+ Copyright (C) 2013 Florian Franzen
+ 
+ ------------------------------------------------------------------
+ 
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+ 
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+ 
+ You should have received a copy of the GNU General Public License
+ along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ 
+ */
+ 
 #include <H5Cpp.h>
 #include "HDF5Recording.h"
 
