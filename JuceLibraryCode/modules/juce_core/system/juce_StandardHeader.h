48a6c655354e7a943f3983365feebc4a463868de
Aaron Cuevas Lopez <aacuelo@teleco.upv.es>
2015-11-09 19:48:46 -0800

    Optimize symbol visibility on Linux

--- JuceLibraryCode/modules/juce_core/system/juce_StandardHeader.h
+++ JuceLibraryCode/modules/juce_core/system/juce_StandardHeader.h
@@ -112,8 +112,8 @@
 #undef max
 #undef min
 
-//Open Ephys addition: For some reason this define doesn't work as an Xcode option
-#if JUCE_MAC
+//Open Ephys addition: For some reason this define doesn't work as an Xcode option. Add it to linux as well
+#if JUCE_MAC || JUCE_LINUX
  #define JUCE_API __attribute__((visibility("default")))
 #endif
 //==============================================================================
