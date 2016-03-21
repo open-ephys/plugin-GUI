7b9078effd741193e7514454ca21f17e7746c76d
Septen <gammerxpower@gmail.com>
2016-03-21 23:18:35 +0300

    Implemented LinearSmoothedValueAtomic class.
    * Implemented LinearSmoothedValueAtomic class based on the
      JUCE 4.0 library' LinearSmoothedValue class but with some improvements
      that were suggested by Timur Doumler during his JUCE Summit 2015
      talk.
    * LinearSmoothedValueAtomic is very convenient to use with float values
      that need linearly smoothed behaviour (like volume, etc.) and which
      are shared between several threads (i.e. audio and GUI).
    * This class guarantees we won't have any race conditions and it will
      remains lock-free because of using std::atomic inside it.

---
+++ Source/Processors/Dsp/LinearSmoothedValueAtomic.h
@@ -0,0 +1,133 @@
+/*
+  ==============================================================================
+
+   This file is part of the JUCE library.
+   Copyright (c) 2015 - ROLI Ltd.
+
+   Permission is granted to use this software under the terms of either:
+   a) the GPL v2 (or any later version)
+   b) the Affero GPL v3
+
+   Details of these licenses can be found at: www.gnu.org/licenses
+
+   JUCE is distributed in the hope that it will be useful, but WITHOUT ANY
+   WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+   A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+
+   ------------------------------------------------------------------------------
+
+   To release a closed-source product which uses JUCE, commercial licenses are
+   available: visit www.juce.com for more information.
+
+  ==============================================================================
+*/
+
+#ifndef JUCE_LINEARSMOOTHEDVALUE_H_INCLUDED
+#define JUCE_LINEARSMOOTHEDVALUE_H_INCLUDED
+
+#include "../PluginManager/OpenEphysPlugin.h"
+#include <atomic>
+
+//==============================================================================
+/**
+    Utility class for linearly smoothed values like volume etc. that should
+    not change abruptly but as a linear ramp, to avoid audio glitches.
+
+    This class was modified by Open Ephys and just an improvement of LinearSmoothedValue
+    class from the JUCE 4.0 library, but with some features made that were suggested
+    by Timur Doumler during his JUCE Summit 2015 talk, to make it lock-free and guarantee
+    that no race conditions will ever occur using it.
+
+    Usage:
+    - set value by calling setValue() method;
+
+    - get value by calling updateTarget() and then getNextValue() method.
+      We need to call updateTarget() because we should suppose the value could be changed before
+      so we need to update it smoothly and then use it.
+*/
+
+//==============================================================================
+template<typename FloatType>
+class PLUGIN_API  LinearSmoothedValueAtomic
+{
+public:
+    /** Constructor. */
+    LinearSmoothedValueAtomic() noexcept
+        : currentValue  (0)
+        , target        (0)
+        , step          (0)
+        , countdown     (0)
+        , stepsToTarget (0)
+    {
+    }
+
+    /** Constructor. */
+    LinearSmoothedValueAtomic (FloatType initialValue) noexcept
+        : currentValue  (initialValue)
+        , target        (initialValue)
+        , step          (0)
+        , countdown     (0)
+        , stepsToTarget (0)
+    {
+    }
+
+    //==========================================================================
+    /** Reset to a new sample rate and ramp length. */
+    void reset (double sampleRate, double rampLengthInSeconds) noexcept
+    {
+        jassert (sampleRate > 0 && rampLengthInSeconds >= 0);
+        stepsToTarget = (int) std::floor (rampLengthInSeconds * sampleRate);
+        currentValue = target;
+        countdown = 0;
+    }
+
+    //==========================================================================
+    /** Set a new target value. */
+    void setValue (FloatType newValue) noexcept
+    {
+        target.store (newValue);
+    }
+
+    //==========================================================================
+    void updateTarget() noexcept
+    {
+        FloatType newTarget = target.load();
+        if (newTarget != currentTarget)
+        {
+            currentTarget = newTarget;
+            countdown = stepsToTarget;
+
+            if (countdown <= 0)
+                currentValue = currentTarget;
+            else
+                step = (currentTarget - currentValue) / (FloatType) countdown;
+        }
+    }
+
+    //==========================================================================
+    /** Compute the next value. */
+    FloatType getNextValue() noexcept
+    {
+        if (countdown <= 0)
+            return currentTarget;
+
+        --countdown;
+        currentValue += step;
+        return currentValue;
+    }
+
+
+private:
+    //==========================================================================
+    std::atomic<FloatType> target;
+
+    FloatType currentValue;
+    FloatType currentTarget;
+    FloatType step;
+
+    int countdown;
+    int stepsToTarget;
+};
+
+
+#endif   // JUCE_LINEARSMOOTHEDVALUE_H_INCLUDED
