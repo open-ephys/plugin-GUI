8d2b4789839714ad7bbfa956dd9ce10672d0b1b2
jsiegle <joshs@alleninstitute.org>
2018-02-18 18:05:10 -0800

    Fix destination channel of copying to temp buffer

--- Source/Processors/AudioNode/AudioNode.cpp
+++ Source/Processors/AudioNode/AudioNode.cpp
@@ -289,7 +289,7 @@ void AudioNode::process(AudioSampleBuffer& buffer)
                     if (samplesToCopyFromOverflowBuffer > 0) // need to re-add samples from backup buffer
                     {
 
-                        tempBuffer->addFrom(i,    // destination channel
+                        tempBuffer->addFrom(0,    // destination channel
                                             0,                // destination start sample
                                             *overflowBuffer,  // source
                                             0,                // source channel
