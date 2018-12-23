51390624e2de8047c0962adfbd0c18bf2067eb6c
Aaron Cuevas Lopez <aacuelo@teleco.upv.es>
2018-12-23 19:39:50 +0100

    Rearrange Binary Format plugin

--- Source/Plugins/BinaryFormat/OpenEphysLib.cpp
+++ Source/Plugins/BinaryFormat/OpenEphysLib.cpp
@@ -22,7 +22,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 #include <PluginInfo.h>
-#include "BinaryRecording.h"
+#include "RecordEngine/BinaryRecording.h"
 #include <string>
 #ifdef WIN32
 #include <Windows.h>
