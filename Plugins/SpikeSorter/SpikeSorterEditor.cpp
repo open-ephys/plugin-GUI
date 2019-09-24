d534813ba24df88622b1ef32e7f3db991387eb35
Aaron Cuevas Lopez <aacuelo@teleco.upv.es>
2019-09-24 23:44:19 +0200

    Disallow changing sample count in spike sorter while acquisition is active

--- Plugins/SpikeSorter/SpikeSorterEditor.cpp
+++ Plugins/SpikeSorter/SpikeSorterEditor.cpp
@@ -316,11 +316,12 @@ void SpikeSorterEditor::buttonEvent(Button* button)
         PopupMenu waveSizeMenu;
         PopupMenu waveSizePreMenu;
         PopupMenu waveSizePostMenu;
+		bool allowSampleChange = !CoreServices::getAcquisitionStatus();
 
-        waveSizePreMenu.addItem(1,"8",true,processor->getNumPreSamples() == 8);
-        waveSizePreMenu.addItem(2,"16",true,processor->getNumPreSamples() == 16);
-        waveSizePostMenu.addItem(3,"32",true,processor->getNumPostSamples() == 32);
-        waveSizePostMenu.addItem(4,"64",true,processor->getNumPostSamples() == 64);
+        waveSizePreMenu.addItem(1,"8",allowSampleChange,processor->getNumPreSamples() == 8);
+		waveSizePreMenu.addItem(2, "16", allowSampleChange, processor->getNumPreSamples() == 16);
+		waveSizePostMenu.addItem(3, "32", allowSampleChange, processor->getNumPostSamples() == 32);
+		waveSizePostMenu.addItem(4, "64", allowSampleChange, processor->getNumPostSamples() == 64);
 
         waveSizeMenu.addSubMenu("Pre samples",waveSizePreMenu);
         waveSizeMenu.addSubMenu("Post samples",waveSizePostMenu);
