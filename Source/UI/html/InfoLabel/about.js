1c73982d2d1ad3e372edfad7ef0687c7a1fbb82a
Josh Siegle <joshs@alleninstitute.org>
2020-08-26 11:18:12 -0700

    Consolidate HTML into one file

--- Source/UI/html/InfoLabel/about.js
+++ Source/UI/html/InfoLabel/about.js
@@ -1,33 +1,3 @@
 document.write(`
-<img src="logo.svg" alt="Open Ephys Logo" style="width: 300px" />
-<br>
-<p>
-  <strong>Open Ephys</strong> is a nonprofit based in Cambridge, Massachusetts, with team members distributed  around the world. Its mission is to advance our understanding of the brain by promoting community ownership of the tools we use to study it.
-</p>
-<p>
-  The <strong>Open Ephys GUI</strong> is free, collaboratively developed, open-source software for scientific research. It includes many features designed to make extracellular electrophysiology data easier to acquire; however, it is not guaranteed to work as advertised. Before you use it for your own experiments, you should test any capabilities you plan to use. The use of a plugin-based architecture provides the flexibility to customize your signal chain, but it also makes it difficult to test every possible combination of processors in advance. Whenever you download or upgrade the GUI, be sure to test your desired configuration in a "safe" environment before using it to collect real data.
-</p>
-<br>
-<h2>Documentation</h2>
-<p>
-  The Open Ephys GUI User Manual can be found at <a href="https://open-ephys.github.io/gui-docs/User%20Manual/index.html">open-ephys.github.io/gui-docs</a>.
-</p>
-<br>
-<h2>Source Code</h2>
-<p>
-<a href="https://github.com/open-ephys/plugin-GUI">plugin-GUI GitHub repository</a>
-</p>
-<br>
-<h2>Publications</h2>
-<p>
-  Any publications based on data collected with this software should cite the following article: <a href="https://iopscience.iop.org/article/10.1088/1741-2552/aa5eea">Open Ephys: an open-source, plugin-based platform for multichannel electrophysiology.</a> Citations remain essential for measuring the impact of scientific software, so be sure to include references to any open-source tools that you use in your research!
-</p>
-<br>
-<h2>Contact</h2>
-<p>
-  For questions, email <strong>info@open-ephys.org</strong> or <a href="https://github.com/open-ephys/plugin-GUI/Issues"> post an issue on GitHub</a>.
-</p>
-<br>
 
-<p><small>© 2020 Open Ephys, Incorporated</small></p>
 `);
