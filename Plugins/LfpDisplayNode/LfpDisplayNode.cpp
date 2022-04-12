8e1a84962517899d5b67417ade37018b132b1dcc
Josh Siegle <joshs@alleninstitute.org>
2022-04-12 10:20:26 -0700

    timestamps --> sampleNumber

--- Plugins/LfpDisplayNode/LfpDisplayNode.cpp
+++ Plugins/LfpDisplayNode/LfpDisplayNode.cpp
@@ -231,7 +231,7 @@ void LfpDisplayNode::handleTTLEvent(TTLEventPtr event)
     const int eventSourceNodeId = event->getChannelInfo()->getSourceNodeId();
     const int eventTime = event->getSampleNumber() - getFirstSampleNumberForBlock(eventStreamId);
 
-    //LOGD("LFP Viewer received: ", eventSourceNodeId, " ", eventId, " ", event->getTimestamp());
+    //LOGD("LFP Viewer received: ", eventSourceNodeId, " ", eventId, " ", event->getSampleNumber(), " ", getFirstSampleNumberForBlock(eventStreamId));
 
     if (eventId == 1)
     {
