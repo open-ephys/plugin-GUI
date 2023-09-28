10106cdf603e688ce03b3480666ba518395b1743
Anjal Doshi <anjaldoshi09@gmail.com>
2023-09-27 17:16:22 -0700

    Add helper methods for creating/accesing parameters in DataThread class

--- Source/Processors/DataThreads/DataThread.h
+++ Source/Processors/DataThreads/DataThread.h
@@ -120,6 +120,111 @@ public:
     /** Returns the address of the DataBuffer that the input source will fill.*/
     DataBuffer* getBufferAddress(int streamIdx) const;
 
+    /** Adds a boolean parameter, which will later be accessed by name*/
+    void addBooleanParameter(Parameter::ParameterScope scope,
+        const String& name,
+        const String& displayName,
+        const String& description,
+        bool defaultValue,
+        bool deactivateDuringAcquisition = false);
+
+    /** Adds an integer parameter, which will later be accessed by name*/
+    void addIntParameter(Parameter::ParameterScope scope,
+        const String& name,
+        const String& displayName,
+        const String& description,
+        int defaultValue,
+        int minValue,
+        int maxValue,
+        bool deactivateDuringAcquisition = false);
+    
+    /** Adds a string parameter, which will later be accessed by name*/
+    void addStringParameter(Parameter::ParameterScope scope,
+        const String& name,
+        const String& displayName,
+        const String& description,
+        String defaultValue,
+        bool deactivateDuringAcquisition = false);
+
+    /** Adds an float parameter, which will later be accessed by name*/
+    void addFloatParameter(Parameter::ParameterScope scope,
+        const String& name,
+        const String& displayName,
+        const String& description,
+        const String& unit,
+        float defaultValue,
+        float minValue,
+        float maxValue,
+        float stepSize,
+        bool deactivateDuringAcquisition = false);
+
+    /** Adds a selected channels parameter, which will later be accessed by name*/
+    void addSelectedChannelsParameter(Parameter::ParameterScope scope,
+        const String& name,
+        const String& displayName,
+        const String& description,
+        int maxSelectedChannels = std::numeric_limits<int>::max(),
+        bool deactivateDuringAcquisition = false);
+    
+    /** Adds a mask channels parameter, which will later be accessed by name*/
+    void addMaskChannelsParameter(Parameter::ParameterScope scope,
+        const String& name,
+        const String& displayName,
+        const String& description,
+        bool deactivateDuringAcquisition = false);
+
+    /** Adds a categorical parameter, which will later be accessed by name*/
+    void addCategoricalParameter(Parameter::ParameterScope scope,
+        const String& name,
+        const String& displayName,
+        const String& description,
+        Array<String> categories,
+        int defaultIndex,
+        bool deactivateDuringAcquisition = false);
+
+    /** Adds a path parameter which holds a path to a folder or file */
+    void addPathParameter(Parameter::ParameterScope scope,
+        const String& name,
+        const String& displayName,
+        const String& description,
+        const String& defaultValue,
+        const StringArray& validFileExtensions,
+        bool isDirectory,
+        bool deactivateDuringAcquisition = true);
+
+    /** Adds a selected stream parameter which holds the currentlu selected stream */
+    void addSelectedStreamParameter(Parameter::ParameterScope scope,
+        const String& name,
+        const String& displayName,
+        const String& description,
+        Array<String> streamNames,
+        const int defaultIndex,
+        bool deactivateDuringAcquisition = true);
+
+    /** Adds a time parameter with microsecond precision in the form HH:MM:SS.sss */
+    void addTimeParameter(Parameter::ParameterScope scope,
+        const String& name,
+        const String& displayName,
+        const String& description,
+        const String& defaultValue = "00:00:00",
+        bool deactivateDuringAcquisition = true);
+
+    /** Adds a notification parameter that notifies parameter value changed */
+    void addNotificationParameter(Parameter::ParameterScope scope,
+        const String& name,
+        const String& displayName,
+        const String& description,
+        bool deactivateDuringAcquisition = false);
+
+    /** Returns a pointer to a parameter with a given name**/
+    Parameter* getParameter(String name) const;
+
+	/** Returns true if an object has a parameter with a given name**/
+	bool hasParameter(String name) const;
+
+    /** Returns a pointer to a parameter with a given name**/
+    Array<Parameter*> getParameters();
+
 protected:
 
     // ** Allows the DataThread to broadcast a message other plugins */
