ac4fddbeb2a3d03b5ba11339ef8385bb02f3b8ef
Benjamin Mao <benjaminmao123@gmail.com>
2024-05-09 18:17:29 -0700

    add databuffer tests

--- Tests/Processors/DataBufferTests.cpp
+++ Tests/Processors/DataBufferTests.cpp
@@ -13,14 +13,14 @@ TEST(DataBufferTest, CopyToAudioBuffer)
 {
     // Create a Data Buffer with 2 channels and 10 samples
     DataBuffer dataBuffer(2, 10);
+    constexpr int numItems = 10;
 
     {
         // Add data to the buffer
-        float data[] = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
-        int64 sampleNumbers[] = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
-        double timestamps[] = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
-        uint64 eventCodes[] = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
-        int numItems = 10;
+        float data[numItems] = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+        int64 sampleNumbers[numItems] = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+        double timestamps[numItems] = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+        uint64 eventCodes[numItems] = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
 
         dataBuffer.addToBuffer(data, sampleNumbers, timestamps, eventCodes, numItems);
     }
@@ -29,9 +29,19 @@ TEST(DataBufferTest, CopyToAudioBuffer)
     AudioBuffer<float> audioBuffer(2, 10);
 
     // Copy data from the Data Buffer to the Audio Buffer
-    //float* data = 
-    //dataBuffer.readAllFromBuffer(audioBuffer, );
+    float data[numItems];
+    int64 sampleNumbers[numItems];
+    double timestamps[numItems];
+    uint64 eventCodes[numItems];
+    std::optional<int64> timestampSampleIndex;
 
+    dataBuffer.readAllFromBuffer(audioBuffer, sampleNumbers, timestamps, eventCodes, numItems, &timestampSampleIndex);
+
+    for (int i = 0; i < numItems; ++i)
+    {
+        EXPECT_EQ(audioBuffer.getSample(0, i), i);
+        EXPECT_EQ(audioBuffer.getSample(1, i), i);
+    }
     //// Verify that the data was copied successfully
     //for (int channel = 0; channel < audioBuffer.getNumChannels(); ++channel)
     //{
