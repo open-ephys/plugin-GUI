c4dfb05c7fc40711917da8603a19f84ee426914b
Benjamin Mao <benjaminmao123@gmail.com>
2024-05-24 10:53:33 -0700

    Update MidiBufferTests.cpp

--- Tests/Processors/MidiBufferTests.cpp
+++ Tests/Processors/MidiBufferTests.cpp
@@ -2,12 +2,64 @@
 
 #include <ProcessorHeaders.h>
 
+class FakeProcessor : public GenericProcessor
+{
+public:
+    FakeProcessor() :
+        GenericProcessor("FakeProcessor")
+    {
+    }
+
+    // Inherited via GenericProcessor
+    void process(AudioBuffer<float>& continuousBuffer) override
+    {
+    }
+};
+
+
 class MidiBufferTests : public testing::Test
 {
 protected:
     void SetUp() override
     {
+        // Create a DataStream
+        DataStream::Settings dataStreamSettings{
+            "DataStream",
+            "description",
+            "identifier",
+            20000.0f
+        };
 
+        mDataStream = std::make_unique<DataStream>(dataStreamSettings);
+
+        // Create a ContinuousChannel and add it to the DataStream
+        ContinuousChannel::Settings continuousChannelSettings{
+            ContinuousChannel::Type::ELECTRODE,
+            "CH0",
+            "0",
+            "identifier",
+            1.0f,
+            mDataStream.get()
+        };
+
+        mContinuousChannel = std::make_unique<ContinuousChannel>(continuousChannelSettings);
+        mDataStream->addChannel(mContinuousChannel.get());
+
+        // Set the source node id for the data stream
+        mNodeId = 0;
+        mDataStream->setNodeId(mNodeId);
+
+        mEventChannel = std::make_unique<EventChannel>(EventChannel::Settings{
+                EventChannel::Type::TTL, "TTL", "Event", "identifier.ttl", mDataStream.get()
+            });
+
+        // Add the processor to the EventChannels
+        mProcessor = std::make_unique<FakeProcessor>();
+        mProcessor->setNodeId(mNodeId);
+
+        mEventChannel->addProcessor(mProcessor.get());
+
+        mEvent = TTLEvent::createTTLEvent(mEventChannel.get(), 0, 0, true);
     }
 
     void TearDown() override
@@ -16,6 +68,13 @@ protected:
     }
 
 protected:
+    EventPtr mEvent;
+    std::unique_ptr<FakeProcessor> mProcessor;
+    std::unique_ptr<DataStream> mDataStream;
+    std::unique_ptr<ContinuousChannel> mContinuousChannel;
+    std::unique_ptr<EventChannel> mEventChannel;
+
+    int mNodeId;
 };
 
 /*
@@ -24,7 +83,13 @@ This test verifies that multiple Metadata objects inserted into the Midi Buffer 
 */
 TEST_F(MidiBufferTests, ReadWrite)
 {
-    
+    size_t size = mEvent->getChannelInfo()->getDataSize() +
+        mEvent->getChannelInfo()->getTotalEventMetadataSize() + EVENT_BASE_SIZE;
+    HeapBlock<uint8> buffer(size);
 
-    EventPacket packet();
+    EventPacket packet(buffer, size);
+
+    MidiBuffer midiBuffer;
+    midiBuffer.addEvent(packet, 0);
+    midiBuffer.addEvent(packet, 0);
 }
