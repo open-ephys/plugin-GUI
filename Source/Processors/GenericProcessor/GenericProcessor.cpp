4558e25f8bdde44fad309c87d5611688e72672cb
Josh Siegle <joshs@alleninstitute.org>
2024-09-10 12:26:31 -0700

    Remove debug output

--- Source/Processors/GenericProcessor/GenericProcessor.cpp
+++ Source/Processors/GenericProcessor/GenericProcessor.cpp
@@ -1325,9 +1325,6 @@ int GenericProcessor::processEventBuffer()
                 uint32 nSamples = *reinterpret_cast<const uint32*> (dataptr + 24);
                 int64 initialTicks = *reinterpret_cast<const int64*> (dataptr + 28);
 
-                if (startSamplesForBlock[sourceStreamId] > startSample)
-                    std::cout << "GET: " << getNodeId() << " " << sourceStreamId << " " << startSamplesForBlock[sourceStreamId] << " " << startSample << std::endl;
-
                 startSamplesForBlock[sourceStreamId] = startSample;
                 startTimestampsForBlock[sourceStreamId] = startTimestamp;
                 syncStreamIds[sourceStreamId] = syncStreamId;
