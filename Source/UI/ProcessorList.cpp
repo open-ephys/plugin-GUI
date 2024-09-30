d3b827cd4d58e7e5816e5636df70025256e9c564
Anjal Doshi <anjaldoshi09@gmail.com>
2024-09-30 13:07:44 -0700

    ProcessorList: fix drag image rendering on Windows

--- Source/UI/ProcessorList.cpp
+++ Source/UI/ProcessorList.cpp
@@ -457,12 +457,15 @@ void ProcessorList::mouseDrag (const MouseEvent& e)
 
                         LOGA ("Processor List - ", listItem->getName(), " drag start.");
 
-                        Graphics g (dragImage.getImage());
-                        g.setColour (getLookAndFeel().findColour (listItem->colourId));
-                        g.fillAll();
-                        g.setColour (Colours::white);
-                        g.setFont (FontOptions (14.0f));
-                        g.drawSingleLineText (listItem->getName(), 10, 12);
+                        // Draw the drag image
+                        {
+                            Graphics g (dragImage.getImage());
+                            g.setColour (getLookAndFeel().findColour (listItem->colourId));
+                            g.fillAll();
+                            g.setColour (Colours::white);
+                            g.setFont (FontOptions (14.0f));
+                            g.drawSingleLineText (listItem->getName(), 10, 12);
+                        }
 
                         dragImage.getImage().multiplyAllAlphas (0.6f);
 
